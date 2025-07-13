071617feb754cf04d1c7dd093c655e3d5473534f
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2025-07-13 22:14:34 +0700

    fix cosmetic lombok errors

--- backend/src/main/java/com/javarecipe/backend/admin/service/AdminCategoryServiceImpl.java
+++ backend/src/main/java/com/javarecipe/backend/admin/service/AdminCategoryServiceImpl.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@SuppressWarnings({"lombok", "unchecked"})
 public class AdminCategoryServiceImpl implements AdminCategoryService {
 
     private final CategoryRepository categoryRepository;
